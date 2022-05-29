243fdfd62bcfdc366a340bc413b7fe1b1b73e5e9
Saif Al Jabry <68194253+saifprogram7@users.noreply.github.com>
2022-05-29 11:37:10 +0100

    Add files via upload

---
+++ calculation-float.py
@@ -0,0 +1,26 @@
+from math import *
+#Adding Float Value
+firstnumber = 12.56
+secondnumber = 15.17
+result = firstnumber + secondnumber
+print(result)
+#Substract Float Value
+minusfirstnumber = 72.50
+minussecondnumber = 37.70
+minusresult = minusfirstnumber - minussecondnumber
+print(minusresult)
+#Multiply Float Value
+multiplyfirstnumber = 2.5
+multiplysecondnumber = 1.2
+multiplyresult = multiplyfirstnumber * multiplysecondnumber
+print(multiplyresult)
+#Divide Float Value
+dividefirstnumber = 3.5
+dividesecondnumber = 1.5
+divideresult = round(dividefirstnumber / dividesecondnumber)
+print(divideresult)
+#Multiply Float Value with Integer
+multiplyfloatintegerfirstnumber = 2.5
+multiplyfloatintegersecondnumber = 3
+multiplyfloatintegerresult = multiplyfloatintegerfirstnumber * multiplyfloatintegersecondnumber
+print(multiplyfloatintegerresult)
